91ab558e291e0c2715715312e057b91fe438d659
sushil kumar <skp952580@gmail.com>
2026-05-11 01:45:28 +0530

    v1

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express();
 
 
 app.get('/',(req,res)=>{
-     res.send({message:"good, i am running"})
+     res.send({message:"good, i am running v1"})
 })
 
 app.listen(3000,()=>{
-    console.log("runing on the 3000 port")
+    console.log("runing on the 3000 port");
 })
